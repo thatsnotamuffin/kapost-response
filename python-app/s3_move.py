45eddeb4c5b1fe2a55d0f8acd779319dde8f5a82
EC2 Default User <ec2-user@ip-172-31-18-105.ec2.internal>
2019-10-08 05:19:45 +0000

    Initial test of s3_move.py to verify it can see the objects in a bucket as well as their file size

--- python-app/s3_move.py
+++ python-app/s3_move.py
@@ -3,3 +3,27 @@
 # Import libraries
 import boto3
 import sys
+
+# Connect S3
+s3 = boto3.resource('s3')
+s3Client = boto3.client('s3')
+
+# List S3 Buckets
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# Variables
+bucket1 = s3.Bucket(sys.argv[1])
+destinationBucket = s3.Bucket(sys.argv[2])
+threshold = sys.argv[3]
+sourceBucket = sys.argv[1]
+
+# Find all objects in argument 1 / source bucket
+for file in bucket1.objects.all():
+    print(file.key)
+    fileSize = s3Client.head_object(
+        Bucket=sourceBucket,
+        Key=file.key
+    )
+    print(fileSize['ContentLength'])
+
